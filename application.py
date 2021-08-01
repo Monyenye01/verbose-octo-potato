699ccf4073ca860a7dfcec05ff22e40b16581a28
Monyenye01 <78760890+Monyenye01@users.noreply.github.com>
2021-08-01 23:55:19 +0300

    Second

--- application.py
+++ application.py
@@ -1,12 +1,18 @@
+import os
+
 from flask import Flask, flash, redirect, render_template, request, session
-import sqlite3
 from flask_session import Session
+import sqlite3
 
 # Configure application
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print("yes")
     return render_template("index.html")
 
-
+@app.route("/cakes")
+def cakes():
+    print("yes")
+    return render_template("cakes.html")
